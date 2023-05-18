6f9c10e3105361ec66417c71e8794fb553e1f5eb
JackCT1 <jackthomas933@gmail.com>
2023-05-18 20:35:11 +0100

    query selectors declared

---
+++ app.js
@@ -0,0 +1,4 @@
+const squares = document.querySelectorAll(".square");
+const mole = document.querySelector(".mole");
+const score = document.querySelector("#score");
+const timeLeft = document.querySelector("#time-left");

d80cbb37954dc59650bb17b3800e0bdbe0973026
JackCT1 <jackthomas933@gmail.com>
2023-05-19 22:20:40 +0100

    call move mole function

--- app.js
+++ app.js
@@ -13,34 +13,36 @@ function loadMoleOnRandomSquare() {
     square.classList.remove("mole");
   });
 
-  let randomSquare = squares[Math.floor(Math.random * squares.length)];
+  let randomSquare = squares[Math.floor(Math.random() * squares.length)];
   randomSquare.classList.add("mole");
   moleSquareId = randomSquare.id;
-
-  squares.forEach((square) => {
-    square.addEventListener("mousedown", () => {
-      if (square.id === moleSquareId) {
-        score++;
-        scoreDisplay.textContent = score;
-        moleSquareId = null;
-      }
-    });
-  });
 }
+
+squares.forEach((square) => {
+  square.addEventListener("mousedown", () => {
+    if (square.id === moleSquareId) {
+      score++;
+      scoreDisplay.textContent = score;
+      moleSquareId = null;
+    }
+  });
+});
 
 function moveMole() {
-  timerId = setInterval(loadMoleOnRandomSquare, 500);
+  timerId = setInterval(loadMoleOnRandomSquare(), 500);
 }
+
+moveMole();
 
 function timerCountDown() {
   timeLeft--;
   timeLeftDisplay.textContent = timeLeft;
 
   if (timeLeft === 0) {
-    clearInterval(countDowntimerId);
+    clearInterval(countDownTimerId);
     clearInterval(timerId);
     alert("TIME'S UP! Your final score is " + score);
   }
 }
 
-moveMole();
+let countDownTimerId = setInterval(timerCountDown(), 1000);

7f6e6b5619766b9009db601155e98314ca96e6ca
JackCT1 <jackthomas933@gmail.com>
2023-05-19 15:59:23 +0100

    load mole function

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 const squares = document.querySelectorAll(".square");
 const mole = document.querySelector(".mole");
-const score = document.querySelector("#score");
+const scoreDisplay = document.querySelector("#score");
 const timeLeft = document.querySelector("#time-left");
+
+let score = 0;
+let moleSquareId;
+
+function loadMoleOnRandomSquare() {
+  squares.forEach((square) => {
+    square.classList.remove("mole");
+  });
+
+  let randomSquare = squares[Math.floor(Math.random * squares.length)];
+  randomSquare.classList.add("mole");
+  moleSquareId = randomSquare.id;
+
+  squares.forEach((square) => {
+    square.addEventListener("mousedown", () => {
+      if (square.id === moleSquareId) {
+        score++;
+        scoreDisplay.textContent = score;
+        moleSquareId = null;
+      }
+    });
+  });
+}
